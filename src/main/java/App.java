fd054f11ab0ddb812417ba6e4820bdff6c7d5e63
Kristina <sharipovakristina@gmail.com>
2024-06-26 23:15:15 +0300

    Работа с объектами в Spring

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,18 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean2  =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.getMessage());
+
+        Cat cat1 = applicationContext.getBean("cat", Cat.class);
+        System.out.println(cat1.getName());
+
+        Cat cat2 = applicationContext.getBean("cat", Cat.class);
+        System.out.println(cat2.getName());
+
+        System.out.println(bean == bean2);
+        System.out.println(cat1 == cat2);
     }
 }
---
+++ src/main/java/Cat.java
@@ -0,0 +1,30 @@
+public class Cat {
+    private String name;
+
+    public Cat(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
